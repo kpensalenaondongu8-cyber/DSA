fc0e8c82401628c0b17b1777b963397aab321b8a
kpensalenaondongu8-cyber <kpensalenaondongu8@gmail.com>
2026-09-23 22:34:04 +0100

    copy

--- reverse/reverse_list.py
+++ reverse/reverse_list.py
@@ -1,8 +1,22 @@
-def reverse_list(items):
-      new_list = []
-      for i in range(len(items)-1, -1, -1):
-            new_list.append(items[i])
-      return new_list
 
-items = [1, 2, 3, 4, 5]
-print(reverse_list(items))    
+def add_tag(profile, tag):
+    updated = profile.copy()
+    all = updated["tags"][:]
+    updated["tags"] = all
+    all.append(tag)
+    return updated
+
+#  new_list = {"name":profile["name"], "tags":[tag],}
+#     for tag in profile["tags"]:
+#       new_list["tags"].append(tag)
+   
+#     return new_list
+original = {"name": "Ada", "tags": ["python", "key"]}
+changed = add_tag(original, "testing")
+
+print(original)
+print(changed)
+print(original["tags"])
+print(changed["tags"])
+print(changed is original)
+print(changed["tags"] is original["tags"])
