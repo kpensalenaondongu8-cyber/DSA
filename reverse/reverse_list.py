b126e35cfd3c8921ca7e563405c50a895d27282f
kpensalenaondongu8-cyber <kpensalenaondongu8@gmail.com>
2026-09-24 21:53:31 +0100

    reversing a list

--- reverse/reverse_list.py
+++ reverse/reverse_list.py
@@ -1,35 +1,10 @@
+def reverse(items):
 
-# def add_tag(profile, tag):
-#     updated = profile.copy()
-#     all = updated["tags"][:]
-#     updated["tags"] = all
-#     all.append(tag)
-#     return updated
+    new_list = []
 
-#  new_list = {"name":profile["name"], "tags":[tag],}
-#     for tag in profile["tags"]:
-#       new_list["tags"].append(tag)
-   
-#     return new_list
-# original = {"name": "Ada", "tags": ["python", "key"]}
-# changed = add_tag(original, "testing")
+    for item in range(len(items)-1, -1, -1):
+        new_list.append(items[item])
+    return new_list
 
-# print(original)
-# print(changed)
-# print(original["tags"])
-# print(changed["tags"])
-# print(changed is original)
-# print(changed["tags"] is original["tags"])
-# assert original["tags"] == ["python"]
-# assert changed["tags"] == ["python", "testing"]
-# assert set(changed["tags"]) == {"python", "testing"}
-
-# changed["tags"].append("more")
-# assert original["tags"] == ["python"]
-
-
-def ticket_total(price, quantity):
-    total = price * quantity
-    print(total)
-amount = ticket_total("7", 3)
-print(amount)
+all = [1, 2, 3, 4, 5, 6]
+print(reverse(all))        
